f2d284be886b99696a0a4a6d1d78f02877329d48
Ricardo Marques <rimarques@suse.com>
2018-02-27 09:51:54 +0000

    mgr/dashboard_v2: Add DimlessBinaryDirective
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

---
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/shared/directives/dimless-binary.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DimlessBinaryDirective } from './dimless-binary.directive';
+
+describe('DimlessBinaryDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DimlessBinaryDirective(null, null, null);
+    expect(directive).toBeTruthy();
+  });
+});
